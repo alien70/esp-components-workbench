5ec238d374539d8c1b1e23bebeb3343bbd4de50d
Maurizio Attanasi <maurizio.attanasi@hotmail.com>
2019-10-06 16:55:31 +0200

    [master] - added set_env command

--- test_app/main/main.c
+++ test_app/main/main.c
@@ -4,6 +4,8 @@
 #include "driver/gpio.h"
 #include "driver/uart.h"
 
+#define LED (GPIO_NUM_2)
+
 #define AUX (GPIO_NUM_18) // Purple
 #define M0 (GPIO_NUM_19)  // Grey
 #define M1 (GPIO_NUM_27)  // White
@@ -13,8 +15,25 @@
 #define LORA_RTS (UART_PIN_NO_CHANGE)
 #define LORA_CTS (UART_PIN_NO_CHANGE)
 
+void led_task(void *argc)
+{
+    int level = 0;
+    for (;;)
+    {
+        level = gpio_get_level(AUX);
+        gpio_set_level(LED, level);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+}
+
+/**
+ * @brief ESP32 Workbench
+ * 
+ */
 static void setup()
 {
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+
     gpio_set_direction(M0, GPIO_MODE_OUTPUT);
     gpio_set_direction(M1, GPIO_MODE_OUTPUT);
     gpio_set_direction(AUX, GPIO_MODE_INPUT);
@@ -29,17 +48,23 @@ static void setup()
     set_uart_baud_rate(_9600);
     set_parity(_8N1);
     set_uart_port(UART_NUM_2);
-
+    
     ebyte_init();
+
+    // set_uart_baud_rate(_115200);
+    // write_parameters();
+    xTaskCreate(led_task, "led_task", 1024, NULL, 10, NULL);
 }
 
 void app_main(void)
 {
     setup();
 
-    printf("Model: %02x\n", get_model());
+    printf("\nModel: %02x\n", get_model());
     printf("Version: %02x\n", get_version());
-    printf("Features: %02x\n", get_features());
+    printf("Features: %02x\n\n", get_features());
 
-    // xTaskCreate(dummy_task, "dummy_task", 1024, NULL, 10, NULL);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    // ebyte_reset();
 }
